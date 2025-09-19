e3470f2d582c9b476e3e99f02fd33259953b99de
elgerardor <21371638+elgerardor@users.noreply.github.com>
2025-09-18 23:42:30 -0600

    Initial commit

---
+++ src/models/question_schemas.py
@@ -0,0 +1,28 @@
+from typing import List
+from pydantic import BaseModel, Field, validator
+
+class MCQQuestion(BaseModel):
+
+    question: str =  Field(description = "The question text")
+
+    options: List[str] = Field(description = "List of 4 options")
+
+    correct_answer: str = Field(description = "The correct answer from the options")
+
+    @validator('question' ,  pre = True)
+    def clean_question(cls, v):
+        if isinstance(v,dict):
+            return v.get('description', str (v))
+        return str(v)
+    
+class FillBlankQuestion(BaseModel):
+
+    question: str = Field(description = "The quesiton text with '___' for the blank")
+
+    answer: str = Field(description = "The correct wrd or phrase for the blank")
+
+    @validator('question' ,  pre = True)
+    def clean_question(cls, v):
+        if isinstance(v,dict):
+            return v.get('description', str (v))
+        return str(v)
